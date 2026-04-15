9af8694b42e1c3ebc302c62ac8e90b5b35806089
jacksonhendershott <jacksonh.dot@gmail.com>
2026-04-14 19:04:20 -0700

    Extend functionality

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-function removeShorts(link) {
-    if (!link) return;
-    link.remove();
+function removeShortsLink(node) {
+    const section = node.closest('ytd-rich-section-renderer');
+    if (section) {
+        section.remove();
+    }
+}
+
+function scanAndRemoveShorts(root) {
+    root.querySelectorAll('ytd-rich-shelf-renderer[is-shorts]').forEach(node => {
+        removeShortsLink(node);
+    })
+
+    root.querySelectorAll('a[title=Shorts]').forEach(node => {
+        node.closest('ytd-guide-entry-renderer', 'ytm-guide-entry-renderer')?.remove();
+    })
 }
 
 const observer = new MutationObserver((mutations) => {
     for (const mutation of mutations) {
         for (const node of mutation.addedNodes) {
-            if (node instanceof Element && node.tagName === "A" && node.getAttribute("title") === "Shorts") {
-                const parentElement = node.parentElement;
-                if (parentElement != null) {
-                    removeShorts(parentElement);
-                }
+            if (!(node instanceof Element)) continue;
+            if (node.tagName === 'YTD-RICH-SECTION-RENDERER') {
+                const shelf = node.querySelector('ytd-rich-shelf-renderer[is-shorts]');
+                if (shelf) node.remove();
+                continue;
             }
+
+            scanAndRemoveShorts(node);
         }
     }
 })
