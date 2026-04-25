c64ed6ce4821751472f348e88468a5d9ca355ded
jacksonhendershott <jacksonh.dot@gmail.com>
2026-04-25 11:36:13 -0700

    Convert to Tampermonkey format

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-function removeShortsLink(node) {
-    const section = node.closest('ytd-rich-section-renderer');
-    if (section) {
-        section.remove();
-    }
-}
+// ==UserScript==
+// @name         No YouTube Shorts
+// @namespace    https://jacksonh.net/
+// @version      2026-04-25
+// @description  Makes YouTube Shorts harder to get to.
+// @author       Jackson
+// @match        https://www.youtube.com/*
+// @icon         https://github.com/jacksonhendershott/noyoutubeshorts/blob/9c450f649e6b396d95befa1f03bfd48e121424a7/images/icon-128.png?raw=true
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
 
-function scanAndRemoveShorts(root) {
-    root.querySelectorAll('ytd-rich-shelf-renderer[is-shorts]').forEach(node => {
-        removeShortsLink(node);
-    })
+(function() {
+    'use strict';
 
-    root.querySelectorAll('a[title=Shorts]').forEach(node => {
-        node.closest('ytd-guide-entry-renderer', 'ytm-guide-entry-renderer')?.remove();
-    })
-}
-
-const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-        for (const node of mutation.addedNodes) {
-            if (!(node instanceof Element)) continue;
-            if (node.tagName === 'YTD-RICH-SECTION-RENDERER') {
-                const shelf = node.querySelector('ytd-rich-shelf-renderer[is-shorts]');
-                if (shelf) node.remove();
-                continue;
-            }
-
-            scanAndRemoveShorts(node);
+    function removeShortsLink(node) {
+        const section = node.closest('ytd-rich-section-renderer');
+        if (section) {
+            section.remove();
         }
     }
-})
 
-observer.observe(document.querySelector('ytd-app'), {
-    childList: true,
-    subtree: true
-});
+    function scanAndRemoveShorts(root) {
+        root.querySelectorAll('ytd-rich-shelf-renderer[is-shorts], ytd-reel-shelf-renderer').forEach(node => {
+            removeShortsLink(node);
+        })
 
-console.log("[NYS] Loaded!");
+        root.querySelectorAll('a[title="Shorts"]').forEach(node => {
+            node.closest('ytd-guide-entry-renderer, ytm-guide-entry-renderer')?.remove();
+        })
+    }
+
+    function init() {
+        scanAndRemoveShorts(document);
+        const observer = new MutationObserver((mutations) => {
+            for (const mutation of mutations) {
+                for (const node of mutation.addedNodes) {
+                    if (!(node instanceof Element)) continue;
+                    if (node.tagName === 'YTD-RICH-SECTION-RENDERER') {
+                        const shelf = node.querySelector('ytd-rich-shelf-renderer[is-shorts]');
+                        if (shelf) node.remove();
+                        continue;
+                    }
+
+                    scanAndRemoveShorts(node);
+                }
+            }
+        })
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+
+        console.log("[NYS] Loaded!");
+    }
+
+    init();
+})();
